b277dfa0a3d2d102d10b11fce617b56029483329
Carlos <nzlosh@yahoo.com>
2017-03-27 20:39:15 +0200

    Changed entry point to work with stackstorm 2.2

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ except ImportError:
 
 setup(
     name="yaql_json",
-    version="0.1.14",
+    version="0.1.15",
     author = "Carlos",
     author_email = "nzlosh@yahoo.com",
     packages=find_packages(),
     install_requires=["mistral", "yaql"],
     entry_points={
-        "mistral.yaql_functions": [
+        "mistral.expression.functions": [
             "to_json = yaql_json.json_converter:to_json",
             "from_json = yaql_json.json_converter:from_json"
         ]
